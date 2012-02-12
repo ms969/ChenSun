ff181d9021afa512f34fe396552338c8ef451400
MJ Sun <ms969@cornell.edu>
2012-02-12 17:42:48 -0500

    Set up server/client backbone

--- CS5430/src/client/ClientGUI.java
+++ CS5430/src/client/ClientGUI.java
@@ -1,5 +1,46 @@
 package client;
 
-public class ClientGUI {
+import java.io.*;
+import java.net.*;
 
+public class ClientGUI {
+	public static void main(String[] args) throws IOException {
+
+		Socket kkSocket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+
+		try {
+			// connecting to socket 4443
+			kkSocket = new Socket(InetAddress.getLocalHost(), 4443);
+			out = new PrintWriter(kkSocket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(
+					kkSocket.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host: Localhost.");
+			System.exit(1);
+		} catch (IOException e) {
+			System.err
+					.println("Couldn't get I/O for the connection to: Local Host.");
+			System.exit(1);
+		}
+		System.out.println("Client initiated.");
+		String fromServer;
+		
+		while ((fromServer = in.readLine()) != null) {
+			System.out.print("Server: ");
+			System.out.println(fromServer);
+			if (fromServer.equals("This is server.")) {
+				System.out.println("Client: This is client.");
+				out.println("This is client.");
+			}
+			if (fromServer.equals("Connected.")) {
+				break;
+			}
+		}
+
+		out.close();
+		in.close();
+		kkSocket.close();
+	}
 }
--- CS5430/src/server/SocialNetworkServer.java
+++ CS5430/src/server/SocialNetworkServer.java
@@ -1,5 +1,54 @@
 package server;
 
-public class SocialNetworkServer {
+import java.net.*;
+import java.io.*;
 
+public class SocialNetworkServer {
+	public static void main(String[] args) throws IOException {
+
+		ServerSocket serverSocket = null;
+		try {
+			// creates the server socket and listens on port 4443
+			serverSocket = new ServerSocket(4443);
+		} catch (IOException e) {
+			System.err.println("Could not listen on port: 4443.");
+			System.exit(1);
+		}
+
+		Socket clientSocket = null;
+		try {
+			// connecting to the client
+			clientSocket = serverSocket.accept();
+		} catch (IOException e) {
+			System.err.println("Accept failed.");
+			System.exit(1);
+		}
+
+		// Getting client socket's output stream
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+		// Getting client socket's input stream
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				clientSocket.getInputStream()));
+		System.out.println("Server initiated.");
+		
+		System.out.println("Server: This is server.");
+		out.println("This is server.");
+		
+		String inputLine;
+		while ((inputLine = in.readLine()) != null) {
+			System.out.print("Client: ");
+			System.out.println(inputLine);
+			if (inputLine.equals("This is client.")) {
+				System.out.println("Server: Connected.");
+				out.println("Connected.");
+				break;
+			}
+		}
+		
+		// closing the connection
+		out.close();
+		in.close();
+		clientSocket.close();
+		serverSocket.close();
+	}
 }
