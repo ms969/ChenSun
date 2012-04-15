3b5427808099fcb2803aeb09d3b76047057a3d3b
kfc35 <chen.kevin.f@gmail.com>
2012-04-15 16:01:36 -0400

    removed those stinky sql files

--- CS5430/src/database/SocialNetworkDatabaseRegions.java
+++ CS5430/src/database/SocialNetworkDatabaseRegions.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SocialNetworkDatabaseRegions {
 	//private static int numPostsPerBoard = 2;
--- CS5430/src/database/initializeFreeForAllTable.sql.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO freeforall.posts
-VALUES (null, "Kevin", NOW(), "We meet again! There are two starts next to this post for me! One star for MJ!", NOW()),
-(null, "MJ", NOW(), "Kevin can only view this. There's no star next to it for him.", NOW()),
-(null, "MJ", NOW(), "Kevin can post under this! There is a single star next to it for him.", NOW());
-INSERT INTO freeforall.postprivileges
-VALUES (1, "MJ", 'viewpost'), (2, "Kevin", 'view'), (3, "Kevin", 'viewpost');
-INSERT INTO freeforall.replies
-VALUES (1, null, "MJ", NOW(), "You're a loser."),
-(3, null, "Kevin", NOW(), "I can't reply to your other post over there... so I'm posting here. Dork.")
